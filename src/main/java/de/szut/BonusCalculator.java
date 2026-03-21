39dfb417a02654314c148c68c1dabdbf47c41bcf
Pico Luetjens <pico.luetjens@hmmh.de>
2026-03-21 13:01:55 +0100

    update BonusCalculator and BonusCalculator Test

--- src/main/java/de/szut/BonusCalculator.java
+++ src/main/java/de/szut/BonusCalculator.java
@@ -5,6 +5,17 @@ public class BonusCalculator {
     private final BonusService bonusService = new BonusService();
 
     public double calculateBonus(Employee employee) {
-        return bonusService.applyBonus(1);
+        Bonus bonus = new BaseBonus(1000);
+
+        bonus = new CompanyServiceBonusDecorator(bonus, employee.companyService());
+        bonus = new ProjectCompletionBonusDecorator(bonus, employee.completedProjects());
+        bonus = new TeamLeaderBonusDecorator(bonus, employee.teamLeader());
+        bonus = new LowAbsenceBonusDecorator(bonus, employee.absentDays());
+        bonus = new HighAbsencePenaltyDecorator(bonus, employee.absentDays());
+        bonus = new PerformanceBonusDecorator(bonus, employee.performancePoints());
+
+        double result = bonus.calculateBonus();
+
+        return bonusService.applyBonus(result);
     }
 }
--- src/test/java/de/szut/BonusCalculatorTest.java
+++ src/test/java/de/szut/BonusCalculatorTest.java
@@ -1,23 +1,60 @@
 package de.szut;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BonusCalculatorTest {
 
-    private final Employee employee = new Employee(
-            "Max Mustermann",
-            5,
-            30,
-            2,
-            10,
-            true
-    );
     private final BonusCalculator bonusCalculator = new BonusCalculator();
 
-    @Test
-    void shouldReturnCalculatedBonus() {
-        assertThat(bonusCalculator.calculateBonus(employee)).isEqualTo(1);
+    @ParameterizedTest
+    @MethodSource("employeeProvider")
+    void shouldReturnCalculatedBonusForGivenEmployee(Employee employee, double expectedBonus) {
+
+        double result = bonusCalculator.calculateBonus(employee);
+
+        assertEquals(expectedBonus, result, "unexpected result");
+    }
+
+    private static Stream<Arguments> employeeProvider() {
+        return Stream.of(
+                Arguments.of(new Employee("Max Mustermann",
+                        5,
+                        30,
+                        2,
+                        10,
+                        true),
+                        2900d
+                ),
+                Arguments.of(new Employee("John Doe",
+                        10,
+                        80,
+                        7,
+                        15,
+                        true),
+                        5000d
+                ),
+                Arguments.of(new Employee("Marcel Davis",
+                        3,
+                        40,
+                        1,
+                        20,
+                        false),
+                        1600d
+                ),
+                Arguments.of(new Employee("Erika Mustermann",
+                        10,
+                        90,
+                        4,
+                        5,
+                        true),
+                        4900d
+                )
+        );
     }
 }
